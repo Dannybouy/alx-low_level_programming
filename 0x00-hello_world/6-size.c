1ca64f83389d30dce1445fed511c6e01e9ad4b05
Dannybouy <danielokpara29@gmail.com>
2022-03-11 07:55:50 -0800

    C_programming commands

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry Point
+ *
+ * The function of this file is to print out the various sizes of data types in C
+ *
+ * return: Always 0 (sucess0
+ */
+int main(void)
+{
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of a int: %d bytes(s)\n", sizeof(int));
+printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %d bytes(s)\n", sizeof(float));
+return (0);
+}
