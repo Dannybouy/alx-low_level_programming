fbb8eb552398e2b6f8ae1ef9ab5a2d701a0cb871
Dannybouy <danielokpara29@gmail.com>
2022-03-14 13:07:24 -0700

    Different Commands for a task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the letter of the alphabet
+ *
+ * Description: prints the letter of the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i = 97;
+
+while(i < 123)
+{
+putchar(i);
+i++
+}
+putchar(10);
+return (0);
+}
